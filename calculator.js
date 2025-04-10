6f0407910a51ccacdc68413d51177b066314964f
NoahhhBHOP <noahhhbhop@gmail.com>
2025-04-10 13:21:30 -0500

    linked buttons on html page to javascript

--- calculator.js
+++ calculator.js
@@ -25,3 +25,37 @@ let divide = function(num1,num2) {
 let operate = function(num1,operator,num2) {
     return operator(num1,num2);
 }
+
+let oneButton = document.getElementById('1');
+oneButton.addEventListener('click', () => console.log(1))
+
+let twoButton = document.getElementById('2');
+twoButton.addEventListener('click', () => console.log(2))
+
+let threeButton = document.getElementById('3');
+
+let fourButton = document.getElementById('4');
+
+let fiveButton = document.getElementById('5');
+
+let sixButton = document.getElementById('6');
+
+let sevenButton = document.getElementById('7');
+
+let eightButton = document.getElementById('8');
+
+let nineButton = document.getElementById('9');
+
+let zeroButton = document.getElementById('0');
+
+let divideButton = document.getElementById('divide');
+
+let multiplyButton = document.getElementById('multiply');
+
+let subtractButton = document.getElementById('subtract');
+
+let addButton = document.getElementById('add');
+
+let decimalButton = document.getElementById('decimal');
+
+let equalsButton = document.getElementById('equals');
